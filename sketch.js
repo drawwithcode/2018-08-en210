bdfd109556376b6feeece1f261da23560f88586c
Ennio Sorrentino <enniosorretntino@gmail.com>
2018-11-21 01:34:34 +0100

    level

--- sketch.js
+++ sketch.js
@@ -1,11 +1,27 @@
+var x,y,z
+
 function preload(){
   // put preload code here
 }
 
 function setup() {
   // put setup code here
+  createCanvas(windowWidth,windowHeight)
 }
 
 function draw() {
-  // put drawing code here
+  noStroke()
+  for(i=0;i<width/10;i++){
+    for(h=0;h<height/10;h++){
+      fill(rotationX/i*3,rotationY/h*3,0)
+      rect(i*width/10,h*height/10,width/10,height/10)
+      push()
+       fill('white')
+       textSize(width/20)
+       text('PUT ON A PLAIN TO TURN OFF',100,100,1000)
+      pop()
+
+    }
+  }
+
 }
